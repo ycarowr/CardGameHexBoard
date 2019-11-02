77d046592f63e146f171396b97a113705110968c
ycarowr <ywrycaro@gmail.com>
2019-11-02 15:39:15 +0200

    Add score with tests

---
+++ Assets/Scripts/Runtime/Score/Editor/ScoreTest.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HexCardGame.Runtime.GameScore;
+using NUnit.Framework;
+using UnityEngine;
+
+
+namespace HexCardGame.Runtime.Test
+{
+    public class ScoreTest : BaseTest, ICreateScore
+    {
+        bool _isCreated;
+        IScore _score;
+
+        public override void TearDown()
+        {
+            _score = null;
+            base.TearDown();
+        }
+
+        public override void Create() => _score = new Score(GetPlayers(), Parameters, Dispatcher);
+        public void OnCreateScore(IScore score) => _isCreated = true;
+
+        [Test]
+        public void ScoreCreated_Test() => Assert.IsTrue(_isCreated);
+
+        [Test]
+        public void AddScore_Test()
+        {
+            var userAmount = 25;
+            var aiAmount = 42;
+            _score.Add(PlayerId.User, userAmount);
+            _score.Add(PlayerId.Ai, aiAmount);
+            Assert.IsTrue(userAmount == _score.GetScoreForPlayer(PlayerId.User));
+            Assert.IsTrue(aiAmount == _score.GetScoreForPlayer(PlayerId.Ai));
+        }
+
+        [Test]
+        public void RemoveScore_Test()
+        {
+            var startAmount = 100;
+            var discountUser = 52;
+            var discountAi = 42;
+            _score.Add(PlayerId.User, startAmount);
+            _score.Add(PlayerId.Ai, startAmount);
+            _score.Remove(PlayerId.User, discountUser);
+            _score.Remove(PlayerId.Ai, discountAi);
+            Assert.IsTrue(startAmount - discountUser == _score.GetScoreForPlayer(PlayerId.User));
+            Assert.IsTrue(startAmount - discountAi == _score.GetScoreForPlayer(PlayerId.Ai));            
+        }
+        
+        [Test]
+        public void AddScoreCorrectPlayer_Test()
+        {
+            var user = PlayerId.User;
+            var ai = PlayerId.Ai;
+            var stateBeforeUser = _score.GetScoreForPlayer(user);
+            var stateBeforeAi = _score.GetScoreForPlayer(ai);
+            
+            var amount = 42;
+            _score.Add(user, amount);
+
+            var stateAfterUser = _score.GetScoreForPlayer(user);
+            var stateAfterAi = _score.GetScoreForPlayer(ai);
+            
+            Assert.IsTrue(stateBeforeUser + amount == stateAfterUser);
+            Assert.IsTrue(stateBeforeAi == stateAfterAi);
+        }
+        
+        [Test]
+        public void RemoveScoreCorrectPlayer_Test()
+        {
+            var user = PlayerId.User;
+            var ai = PlayerId.Ai;
+            var stateBeforeUser = _score.GetScoreForPlayer(user);
+            var stateBeforeAi = _score.GetScoreForPlayer(ai);
+            
+            var amount = 42;
+            _score.Remove(user, amount);
+
+            var stateAfterUser = _score.GetScoreForPlayer(user);
+            var stateAfterAi = _score.GetScoreForPlayer(ai);
+            
+            Assert.IsTrue(stateBeforeUser - amount == stateAfterUser);
+            Assert.IsTrue(stateBeforeAi == stateAfterAi);
+        }
+        
+        IPlayer[] GetPlayers() => new IPlayer[]
+        {
+            new Player(PlayerId.User, Parameters, Dispatcher),
+            new Player(PlayerId.Ai, Parameters, Dispatcher)
+        };
+    }
+}
---
+++ Assets/Scripts/Runtime/Score/Score.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tools.Patterns.Observer;
+using UnityEngine;
+
+namespace HexCardGame.Runtime.GameScore
+{
+    [Event]
+    public interface ICreateScore
+    {
+        void OnCreateScore(IScore score);
+    }
+    
+    public interface IScore
+    {
+        int GetScoreForPlayer(PlayerId id);
+        void Add(PlayerId id, int amount);
+        void Remove(PlayerId id, int amount);
+        void Clear();
+    }
+    
+    public class Score : IScore
+    {
+        readonly Dictionary<PlayerId, int> _register = new Dictionary<PlayerId, int>();
+        IDispatcher Dispatcher { get; }
+        GameParameters Parameters { get; }
+        public Score(IPlayer[] players, GameParameters parameters, IDispatcher dispatcher)
+        {
+            Parameters = parameters;
+            Dispatcher = dispatcher;
+            foreach (var i in players)
+                RegisterPlayer(i.Id);
+            OnCreateScore();
+        }
+
+        void RegisterPlayer(PlayerId id) => _register.Add(id, 0);
+        public void Add(PlayerId id, int amount) => _register[id] += amount;
+        public void Remove(PlayerId id, int amount) => _register[id] -= amount;
+        public int GetScoreForPlayer(PlayerId id) => _register[id];
+        public void Clear() => _register.Clear();
+
+        void OnCreateScore() => Dispatcher.Notify<ICreateScore>(i => i.OnCreateScore(this));
+    }
+}
